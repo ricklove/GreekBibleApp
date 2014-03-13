a2f35413ce0e5b42f3a568cf825968f327f947bc
ricklove <rickandkarenlove@hotmail.com>
2014-03-13 06:05:29 +0200

    Remove unneeded files

--- www/Scripts/typings/yadda/yadda.d.ts
+++
@@ -1,8 +0,0 @@
-﻿interface IYaddaLibrary {
-    given(stepText: string, doResult: (args: any) => void): IYaddaLibrary;
-    when(stepText: string, doResult: (args: any) => void): IYaddaLibrary;
-    then(stepText: string, doResult: (args: any) => void): IYaddaLibrary;
-}
-
-declare var require: any;
-declare var scenarios: any;
